c708b2a148a6803f74a22ea3ccaadd1fb7b109b1
Skial Bainn <skial.bainn@gmail.com>
2015-01-21 14:41:04 +0000

    Minor changes to `load`. Fisel constructor accepts a `DOMCollection` instead of a string. Can pass an optional base path to work from. Added pseudo w3c imports code.

--- src/Fisel.hx
+++ src/Fisel.hx
@@ -34,8 +34,8 @@ using sys.FileSystem;
   * - [ ] Allow resources to be loaded from the web.
   * - [x] Make sure all resource uris end with `html` or `htm`.
   * - [x] Each loaded resource is a Fisel instance.
-  * - [x] Allow HTML not wrapped in `<template></template>`.
-  * - [x] Automatically wrap any HTML not wrapped in `<template></template>`.
+	* - [x] Allow HTML not wrapped in `<template></template>`.
+	* - [x] Automatically wrap any HTML not wrapped in `<template></template>`.
   * - [x] Imported HTML replaces a `<content select="css"/>` which was selected by the `select` attribute.
   * - [x] Any unmatched selectors then search the document in its current state for a match.
   * - [x] Attributes on `<content id="1" data-name="Skial" /> which don't exist on the imported HTML are transfered over.
@@ -73,30 +73,35 @@ class Fisel {
 	private var uri:Uri;
 	private var imports:DOMCollection;
 	private var insertionPoints:DOMCollection;
-	private var importCache:StringMap<Fisel> = new Map();
+	private var importCache:StringMap<Fisel> = new StringMap();
 	
-	public function new(html:String) {
+	public function new(html:DOMCollection, ?path:String) {
 		if (_css == null) _css = new CssParser();
 		if (_html == null) _html = new HtmlParser();
 		if (_selector == null) _selector = new SelectorParser();
 		
-		document = html.parse();
+		document = html;
 		insertionPoints = document.find( 'content[select]' );
 		imports = document.find( 'link[rel*="import"][href*=".htm"]' );
 		var bases = document.find( 'base[href]' );
 		
-		// If no `<base />` is found, set the root uri to the current working directory.
-		if (bases.length == 0) {
-			uri = new Uri( #if !js Sys.getCwd().normalize() #else js.Browser.document.location.host #end );
+		if (path == null) {
+			// If no `<base />` is found, set the root uri to the current working directory.
+			if (bases.length == 0) {
+				uri = new Uri( #if !js Sys.getCwd().normalize() #else js.Browser.document.location.host #end );
+				
+			} else {
+				var _base = bases.collection[0].attr( 'href' ).normalize();
+				_base = !_base.isAbsolute() ? (#if !js Sys.getCwd() #else js.Browser.document.location.host #end + _base).normalize() : _base;
+				uri = new Uri( _base );
+				
+			}
 			
 		} else {
-			var _base = bases.collection[0].attr( 'href' ).normalize();
-			_base = !_base.isAbsolute() ? (#if !js Sys.getCwd() #else js.Browser.document.location.host #end + _base).normalize() : _base;
-			uri = new Uri( _base );
+			uri = new Uri( path );
 			
 		}
 		
-		load( imports );
 	}
 	
 	public function toString():String {
@@ -114,7 +119,9 @@ class Fisel {
 			attr = content.attr( 'select' );
 			
 			if (attr.startsWith('#') && importCache.exists( attr.substring(1) )) {
-				content.replaceWith( importCache.get( attr = attr.substring(1) ).document.innerHTML().htmlUnescape().parse() );
+				content.replaceWith( 
+					importCache.get( attr = attr.substring(1) ).document.find( 'template:first-child' ).innerHTML().htmlUnescape().parse() 
+				);
 				
 			} else {
 				matches = document.find( attr );
@@ -180,7 +187,8 @@ class Fisel {
 		imports.remove();
 	}
 	
-	public function load(imports:DOMCollection):Void {
+	// Act like HTML imports.
+	public function load():Void {
 		var content = '';
 		var attr = '';
 		var id = '';
@@ -189,6 +197,7 @@ class Fisel {
 		for (imp in imports) {
 			attr = imp.attr( 'href' );
 			path = new Uri( '$uri/$attr'.normalize() );
+			
 			// Allow for user defined ids, but default to the file name without its extension.
 			id = imp.attr( 'id' ) != '' ? imp.attr( 'id' ) : attr.withoutExtension().withoutDirectory();
 			
@@ -196,11 +205,13 @@ class Fisel {
 				content = loadFile( '$path' );
 				// All content should be wrapped in `<template></template>`.
 				if (content.indexOf( '<template>' ) == -1) content = '<template>$content</template>';
-				importCache.set( id, new Fisel( content ) );
+				importCache.set( id, new Fisel( content.parse() ) );
 				
 			}
 			
 		}
+		
+		for (key in importCache.keys()) importCache.get( key ).load();
 	}
 	
 	#if !js
@@ -218,4 +229,60 @@ class Fisel {
 	}
 	#end
 	
+	// Untested methods based on w3c html imports spec >see http://w3c.github.io/webcomponents/spec/imports/
+	
+	// http://w3c.github.io/webcomponents/spec/imports/#updateing-branch
+	//private function updatingBranch(document:Fisel):Void {
+		// Need a way to determine if `document` is the master document.
+		// A master document is the html document which kicked off the chain of
+		// imports.
+		/*
+		 * var list = document.linkList;
+		 * for (link in list) {
+		 * 		var location = link.location;
+		 * 		var import = link.location;
+		 * 		// Implement step 3 of #updateing-branch
+		 * 		if (link.branch && import != null) {
+		 * 			updateMarking( import );
+		 * 		}
+		 * }
+		 */
+	//}
+	
+	// http://w3c.github.io/webcomponents/spec/imports/#requesting-import
+	/*private function requestingImport(link:DOMNode, location:String):Void {
+		if (link.attr('async') == 'true') {
+			// mark as async
+		}
+		var document = ''; // Needs to be a Fisel instance.
+		var list = ''; // Needs to fisel.linkList;
+		var item = { link:'', location:'' };
+		// list.push( item );
+		// updateMarking( masterDocument );
+	}
+	
+	// http://w3c.github.io/webcomponents/spec/imports/#fetching-import
+	private function fetchingImport(link:DOMNode, location:String):Fisel {
+		
+	}
+	
+	// http://w3c.github.io/webcomponents/spec/imports/#import-link-tree
+	private function importLinkTree(parent:DOMCollection, current:DOMCollection, pool:Array<DOMCollection>) {
+		// return a tree of documents
+	}*/
+	
 }
+
+// Untested code based on w3c html imports spec.
+
+/*class Requested {
+	public var branch:Bool;
+	public var link:DOMNode;
+	public var location:String;
+	
+	public function new(link:DOMNode, location:String, ?branch:Bool = false) {
+		this.link = link;
+		this.location = location;
+		this.branch = branch;
+	}
+}*/
